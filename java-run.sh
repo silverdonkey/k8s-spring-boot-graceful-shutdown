aa5077c8920611730424cec5f6bd7f485f9a53df
Nikolay Orozov <silverdonkey@users.noreply.github.com>
2021-11-25 10:18:38 +0100

    Update java-run.sh

--- java-run.sh
+++ java-run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-echo "Run scripts before starting your java app... (e.g. import SSL certificates)"
+echo "Using a shell as a pre-start hook."
+echo "You can run scripts before starting your java app... (e.g. import SSL certificates)"
 echo "JAVA_HOME=$JAVA_HOME"
 echo "JAVA_OPTIONS=$JAVA_OPTIONS"
 # exec command is very important here
 # otherwise graceful is not working since
 # only the shell script recieves the SIGERM command
 # but it doesn't propagates it to the java process
-exec /usr/bin/java $JAVA_OPTIONS -jar /app/application.jar $@
+exec $JAVA_HOME/bin/java $JAVA_OPTIONS -jar /app/application.jar $@
