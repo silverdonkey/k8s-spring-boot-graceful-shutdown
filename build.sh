57717e56b051aeb84686d6f600550f1bf3f684f3
Nikolay Orozov <dev@niko-consulting.de>
2024-01-31 18:02:35 +0100

    refactored: better os and arch handling

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#set -eu
+
 help() {
-    echo "Create docker images for Demo"
+    echo "Build the project and create docker images for Demo"
     echo
     echo "Usage $0 <tag>"
     echo "tag: The tag of docker images in the form '1.0.0' or 'latest'"
@@ -16,21 +18,62 @@ REPOSITORY='k8s-spring-boot-app'
 TAG=$1
 PLATFORM_FLAG=""
 
-case `uname -m` in
-    "x86_64"*)
-        SUPPORTED_ARCHITECTURE="x86_64/amd64"
-        echo "You are on $SUPPORTED_ARCHITECTURE"
-        PLATFORM_FLAG=""
+os=$(uname -s)
+arch=$(uname -m)
+OS=${OS:-"${os}"}
+ARCH=${ARCH:-"${arch}"}
+OS_ARCH=""
+
+BIN=${BIN:-crank}
+
+unsupported_arch() {
+  local os=$1
+  local arch=$2
+  echo "This build script does not support $os / $arch at this time."
+  exit 1
+}
+
+case $OS in
+#  CYGWIN* | MINGW64* | Windows*)
+#    if [ $ARCH = "x86_64" ]
+#    then
+#      OS_ARCH=windows_amd64
+#      BIN=crank.exe
+#    else
+#      unsupported_arch $OS $ARCH
+#    fi
+#    ;;
+  Darwin)
+    case $ARCH in
+      x86_64|amd64)
+        OS_ARCH=darwin_amd64
         ;;
-    "arm64"*)
-        SUPPORTED_ARCHITECTURE="arm64"
-        echo "You are on $SUPPORTED_ARCHITECTURE"
+      arm64)
+        OS_ARCH=darwin_arm64
         PLATFORM_FLAG="--platform linux/amd64"
         ;;
-    *)
-        SUPPORTED_ARCHITECTURE="unknown"
-        echo "The build script does not support your system architecture. Please build manually." && exit 1
+      *)
+        unsupported_arch $OS $ARCH
         ;;
+    esac
+    ;;
+  Linux)
+    case $ARCH in
+      x86_64|amd64)
+        OS_ARCH=linux_amd64
+        ;;
+      arm64|aarch64)
+        OS_ARCH=linux_arm64
+        PLATFORM_FLAG="--platform linux/amd64"
+        ;;
+      *)
+        unsupported_arch $OS $ARCH
+        ;;
+    esac
+    ;;
+  *)
+    unsupported_arch $OS $ARCH
+    ;;
 esac
 
 
@@ -45,20 +88,20 @@ function build_maven() {
 }
 
 function build_docker_images() {
-    ARCH=$1
-    PLATFORM_FLAG=$2
+    local arch=$1
+    local platform_flag=$2
     echo
-    echo "Building docker images on $ARCH"
+    echo "Building docker images on $archqq"
     echo
-    docker build $PLATFORM_FLAG -t $REPOSITORY-graceful:$TAG . -f Dockerfile
-    docker build $PLATFORM_FLAG -t $REPOSITORY-non-graceful:$TAG . -f Dockerfile-non-graceful
-    docker build $PLATFORM_FLAG -t $REPOSITORY-graceful-dumb-init:$TAG . -f Dockerfile-graceful-dumb-init
+    docker build $platform_flag -t $REPOSITORY-graceful:$TAG . -f Dockerfile
+    docker build $platform_flag -t $REPOSITORY-non-graceful:$TAG . -f Dockerfile-non-graceful
+    docker build $platform_flag -t $REPOSITORY-graceful-dumb-init:$TAG . -f Dockerfile-graceful-dumb-init
     echo
-    echo "Done! Docker images successfully created on $ARCH"
+    echo "Done! Docker images successfully created on $arch"
     echo
 }
 
 
 
 build_maven
-build_docker_images "$SUPPORTED_ARCHITECTURE" "$PLATFORM_FLAG"
+build_docker_images "$OS_ARCH" "$PLATFORM_FLAG"
